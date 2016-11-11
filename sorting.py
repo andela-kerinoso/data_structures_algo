37c9de1e4bb8c29a250dfe39149eba0016f91868
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-11 22:35:20 +0100

    Implement short bubble sort algorithm with benchmarking

--- sorting.py
+++ sorting.py
@@ -13,13 +13,35 @@ def bubble_sort(arr_list):
     return '%s comparisons and %s exchanges.' % (num_of_comparison, num_of_exchanges) # For bigO benchmarking
 
 
+def short_bubble_sort(arr_list):
+    # For bigO benchmarking
+    num_of_comparison = 0
+    num_of_exchanges = 0
+
+    exchange = True
+    pass_num = len(arr_list) - 1
+
+    while pass_num > 0 and exchange:
+        exchange = False
+        for j in range(pass_num):
+            num_of_comparison += 1 # For bigO benchmarking
+            if arr_list[j] > arr_list[j + 1]:
+                exchange = True
+                arr_list[j], arr_list[j + 1] = arr_list[j + 1], arr_list[j]
+                num_of_exchanges += 1 # For bigO benchmarking
+
+        pass_num -= 1
+
+    return '%s comparisons and %s exchanges.' % (num_of_comparison, num_of_exchanges) # For bigO benchmarking
+
+
 if __name__ == '__main__':
     import timeit
 
     unsorted_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     sorted_list = [17, 20, 26, 31, 44, 54, 55, 77, 93]
 
-    sorting_algos = [bubble_sort,]
+    sorting_algos = [bubble_sort, short_bubble_sort,]
 
     for sorting_algo in sorting_algos:
         print('{}: {} sec, {}'.format(
