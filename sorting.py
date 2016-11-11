2f8eb0a03f245e19f26bb864b15313f7942a7fd4
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-11 22:36:22 +0100

    Implement selection sort algorithm with benchmarking

--- sorting.py
+++ sorting.py
@@ -35,13 +35,31 @@ def short_bubble_sort(arr_list):
     return '%s comparisons and %s exchanges.' % (num_of_comparison, num_of_exchanges) # For bigO benchmarking
 
 
+def selection_sort(arr_list):
+    # For bigO benchmarking
+    num_of_comparison = 0
+    num_of_exchanges = 0
+
+    for pass_num in range(len(arr_list) - 1, 0, -1):
+        pos_max = 0
+        for j in range(1, pass_num):
+            num_of_comparison += 1 # For bigO benchmarking
+            if arr_list[j] > arr_list[pos_max]:
+                pos_max = j
+
+        arr_list[pass_num], arr_list[pos_max] = arr_list[pos_max], arr_list[pass_num]
+        num_of_exchanges += 1 # For bigO benchmarking
+
+    return '%s comparisons and %s exchanges.' % (num_of_comparison, num_of_exchanges) # For bigO benchmarking
+
+
 if __name__ == '__main__':
     import timeit
 
     unsorted_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     sorted_list = [17, 20, 26, 31, 44, 54, 55, 77, 93]
 
-    sorting_algos = [bubble_sort, short_bubble_sort,]
+    sorting_algos = [bubble_sort, short_bubble_sort, selection_sort]
 
     for sorting_algo in sorting_algos:
         print('{}: {} sec, {}'.format(
