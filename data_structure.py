7e908fd2b3cd9be3e2c5917e946e67eb08b25af0
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-17 20:53:43 +0100

    Fix bugs

--- data_structure.py
+++ data_structure.py
@@ -1,20 +1,22 @@
 class Stack(object):
     """Implementation of Stack ADT"""
 
-    def __init__(self, items):
+    def __init__(self, items=[]):
         self.items = items
 
     def is_empty(self):
         return self.items == []
 
     def push(self, item):
-        self.items.appends(item)
+        self.items.append(item)
 
     def pop(self):
-        return self.items.pop()
+        if not self.is_empty():
+            return self.items.pop()
 
     def peek(self):
-        return self.items[len(self.items) - 1]
+        if not self.is_empty():
+            return self.items[len(self.items) - 1]
 
     def size(self):
         return len(self.items)
