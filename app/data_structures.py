36985aad167674aa4be9f18ce6bee6d492c69e4d
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-19 22:48:55 +0100

    Implement a Node

--- app/data_structures.py
+++ app/data_structures.py
@@ -66,3 +66,23 @@ class Deque(object):
 
     def size(self):
         return len(self.items)
+
+
+class Node(object):
+    """Implementation of a Node."""
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, data):
+        self.data = data
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, next):
+        self.next = next
