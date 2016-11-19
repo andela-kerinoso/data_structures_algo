9be52e22e89ce24d5327e980d267fbdf742cdc92
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-19 18:11:47 +0100

    Implement Deque ADT.

--- app/data_structures.py
+++ app/data_structures.py
@@ -41,3 +41,28 @@ class Queue(object):
 
     def size(self):
         return len(self.items)
+
+
+class Deque(object):
+    """Implementation of Deque ADT."""
+
+    def __init__(self, items=[]):
+        self.items = items
+
+    def is_empty(self):
+        return self.items == []
+
+    def add_front(self, item):
+        self.items.append(item)
+
+    def add_rear(self, item):
+        self.items.insert(0, item)
+
+    def remove_front(self):
+        return self.items.pop()
+
+    def remove_rear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
